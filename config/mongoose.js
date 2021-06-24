c5d9b1706f7f9fa7b6be6cdf964360baae228c8d
Riya Agarwal <agarwal.riya71@gmail.com>
2021-06-24 22:42:50 +0530

    Chnages

--- config/mongoose.js
+++ config/mongoose.js
@@ -8,8 +8,8 @@ const db = mongoose.connection;
 
 db.on("error", console.error.bind(console, "Error connecting to mongodb"));
 
-db.once("open", function () {
-  console.log("connected to Database :: MongoDB :: teamsdb");
-});
+// db.once("open", function () {
+//   console.log("connected to Database :: MongoDB :: teamsdb");
+// });
 
 module.exports = db;
